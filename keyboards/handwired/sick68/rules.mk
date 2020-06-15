0a1089a3fc948d71dd17945899042ac982dd6f92
AFprinter <62229696+AFprinter@users.noreply.github.com>
2020-06-15 14:30:38 -0700

    Update rules.mk
    Changed rules to convert from pro micro to qwk photon c.

--- keyboards/handwired/sick68/rules.mk
+++ keyboards/handwired/sick68/rules.mk
@@ -1,5 +1,5 @@
 # MCU name
-MCU = atmega32u4
+MCU = STM32F303
 
 # Bootloader selection
 #   Teensy       halfkay
@@ -9,7 +9,7 @@ MCU = atmega32u4
 #   QMK DFU      qmk-dfu
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
-BOOTLOADER = caterina
+
 
 # Build Options
 #   change yes to no to disable
